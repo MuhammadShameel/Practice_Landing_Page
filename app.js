f7b5b8e8164234dff94b4cfcfdddaed7e6e3e2b3
MuhammadShameel <mshameelkz@gmail.com>
2023-09-19 16:40:14 +0500

    added nav items

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ var swiper = new Swiper(".image-slider", {
   },
 });
 
+window.addEventListener("load", function () {
+  const element = document.querySelector(".fade-in");
+  element.classList.add("active");
+});
+
 let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector(".navbar");
 
